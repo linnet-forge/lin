f7ced470cdb7c513fa71f7361bf332a6f1d7a0c8
strict1parents <hastthis@gmail.com>
2025-09-18 23:18:38 +0200

    added implementation of init func

--- source/command/init.c
+++ source/command/init.c
@@ -1,5 +1,29 @@
-#include <stdio.h>
-
-
-
 #include "../include/command/init.h"
+
+
+LIN_INIT_RES
+init()
+{
+
+        DIR* dir = opendir(".lin");
+
+        if (dir) {
+                printf("The lin system is already initialized");
+                closedir(dir);
+                return LIN_INIT_ALREADY;
+        }
+
+
+        if (mkdir(".lin", 0755) == 0) {
+                printf("The lin system was initialized successfully :)\n");
+                return LIN_INIT_SUCCESS;
+        }
+
+
+        else {
+                perror("The directory could not be created :(");
+                return LIN_INIT_FAILED;
+        }
+
+
+}
