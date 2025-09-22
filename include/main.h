a5894c94edabc8c383c7085cc00e6f02cda85321
strict1parents <hastthis@gmail.com>
2025-09-22 16:20:31 +0200

    buildin the structure/remake of CMake

---
+++ include/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <errno.h>
+
+typedef enum
+{
+
+        LI_SUCCESS = 0,
+        LI_MEDIUM = 1,
+        LI_ERROR = 2,
+
+} LIR;
+
+
+
+#endif
